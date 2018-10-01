5b15d71fb9731f4ffb293baafd1abcefef11ec64
Adrian Demian <adrian@themefy.io>
2018-10-01 15:24:40 +0300

    add ignored sql file

---
+++ test/external_data/invoice.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS "public"."invoice";
+
+CREATE TABLE "public"."invoice" (
+	"id" int8 NOT NULL
+)
+WITH (OIDS=FALSE);
+ALTER TABLE "public"."invoice" OWNER TO "postgres";
+
+BEGIN;
+INSERT INTO "public"."invoice" VALUES ('1');
+INSERT INTO "public"."invoice" VALUES ('2');
+INSERT INTO "public"."invoice" VALUES ('3');
+INSERT INTO "public"."invoice" VALUES ('4');
+INSERT INTO "public"."invoice" VALUES ('5');
+COMMIT;
