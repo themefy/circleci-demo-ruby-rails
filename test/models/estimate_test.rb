8a61df646c969c4754ddce167350a8cf09d734ae
Adrian Demian <adrian@themefy.io>
2018-10-01 15:37:35 +0300

    fix estimate assert - count

--- test/models/estimate_test.rb
+++ test/models/estimate_test.rb
@@ -2,6 +2,6 @@ require 'test_helper'
 
 class EstimateTest < ActiveSupport::TestCase
   test 'count estimates' do
-    assert_equal Estimate.all, 1
+    assert_equal Estimate.all.count, 1
   end
 end
